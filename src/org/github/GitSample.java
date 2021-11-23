e3cf48878898ed4142b46fd482dcd9ea332c0c2a
Aishugitacc <aishuravinagulan19@gmail.com>
2021-11-23 16:02:24 +0530

    FirstStage

---
+++ src/org/github/GitSample.java
@@ -0,0 +1,18 @@
+package org.github;
+
+public class GitSample {
+	private void empid() {
+		System.out.println("7281643735164");
+		
+
+	}
+    private void empname() {
+		System.out.println("Aishwarya");
+
+	}
+    public static void main(String[] args) {
+		GitSample s= new GitSample();
+		s.empid();
+		s.empname();
+	}
+}
